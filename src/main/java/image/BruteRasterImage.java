1040b6df1dd95474a45cd33c0e4f46825babe522
DUPARC Antoine <antoine.duparc@orange.fr>
2018-11-28 11:05:29 +0100

    constructor matrix

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -7,15 +7,31 @@ public class BruteRasterImage implements Image {
 
     private int width;
     private int height;
-    private Color[][] pixels = new Color[width][height];
+    private Color[][] pixels;
 
     public BruteRasterImage(Color color, int width, int height){
         this.width = width;
         this.height = height;
+        pixels = new Color[this.width][this.height];
+
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 pixels[x][y] = color;
             }
         }
+    }
+
+    public BruteRasterImage(Color[][] colors){
+        util.Matrices.requiresNonNull(colors);
+        util.Matrices.requiresNonZeroDimensions(colors);
+        util.Matrices.requiresRectangularMatrix(colors);
+
+        width = util.Matrices.getColumnCount(colors);
+        height = util.Matrices.getRowCount(colors);
+        pixels = colors;
+    }
+
+    public void createRepresentation(){
+
     }
 }
