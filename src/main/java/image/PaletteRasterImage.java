56915e93e074e566181fdd4e5448071016a18329
DUPARC Antoine <antoine.duparc@orange.fr>
2018-11-30 12:06:07 +0100

    corrected constructors palette raster image

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -2,20 +2,26 @@ package image;
 
 import javafx.scene.paint.Color;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PaletteRasterImage implements Image {
 
     private int width;
     private int height;
-    private Color[][] pixels;
+    private int[][] indexesOfColor;
+    private List<Color> palette;
 
     public PaletteRasterImage(Color color, int width, int height){
         this.width = width;
         this.height = height;
-        pixels = new Color[width][height];
+        indexesOfColor = new int[width][height];
+        palette = new ArrayList<Color>();
+        palette.add(color);
 
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                pixels[x][y] = color;
+                indexesOfColor[x][y] = palette.indexOf(color);
             }
         }
     }
@@ -23,7 +29,15 @@ public class PaletteRasterImage implements Image {
     public PaletteRasterImage(Color[][] pixels){
         width = util.Matrices.getRowCount(pixels);
         height = util.Matrices.getColumnCount(pixels);
-        this.pixels = pixels;
+        indexesOfColor = new int[width][height];
+        palette = new ArrayList<Color>();
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                while (!palette.contains(pixels[x][y]))
+                    palette.add(pixels[x][y]);
+                indexesOfColor[x][y] = palette.indexOf(pixels[x][y]);
+            }
+        }
     }
 
     public void createRepresentation(){
